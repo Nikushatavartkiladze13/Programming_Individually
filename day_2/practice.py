39797cafb540ca42dd65a123ec03fae35842363e
nikushatavartkiladze13 <nikatavartkiladze09@icloud.com>
2026-02-01 23:31:38 +0400

    first commit

---
+++ day_2/practice.py
@@ -0,0 +1,64 @@
+# sep = " "
+# end = " \n"
+# f-string 
+
+
+
+# sep
+
+# name = input("enter your name: ")
+
+
+# print("hello",name,"how are you doing?", sep="-")
+
+
+# day = input("enter your born day: ")
+# month = input("enter your born month: ")
+# year = input("enter your born year: ")
+
+# print("sheni dabadebis tarigia " + day,month,year, sep="-")
+
+
+# end
+
+# name = input("enter your name: ")
+
+# print("hello,",name,"how are you doing?", end=" ssabaasasavusahiud \n")
+
+
+
+# f-string 
+
+# name = "nika"
+# age = 16
+# height = 1.80
+
+# print(f"hello, {name} you are {age} years old and your height is {height}")
+
+
+# amindi = True
+# sun = False
+# fuli = True
+
+# print(amindi and sun)
+# print(fuli or sun)
+
+
+
+# title() , capitalize(), strip()
+
+name = input("enter your name and surname: ")
+name = name.strip()
+# name = name.capitalize() - x
+name = name.title()
+# როცა strip ფუნქციის გარეშე დავბეჭდეთ ჩვენ ტერმინალში დავამატეთ შეყვანილ ინფორმაციას სფეისები რამაც ტექსტში გამოიწვია არასიზუსტე და პროგრამა არ გამოვიდა მომხიბვლელი მომხმარებლისთვის ამმის გამოსასწორებლად ვიყენებთ strip ფუნქციაას რადგან მომხმარებელმა შეიძლება არ გამოიყენოს პროგრამა დანიშნულების და წესების მიხედვით .
+# ჩვენ გვინდა რომ სახელის და გვარის პირველი ასოები იყოს დიდი შრიფტით მაგრამ ამის გასაკეთებლად ვერ დაგვეხმარება  capitalize რადგან ეს ფუნქცია მხოლოდ მუშაობს ტექსტის პირველ სიტყვაზე და ადიდებს პირველი სიტყვის პირველ ასოს მხოლოდ , ამიტომაც ჩვენ გამოვიყენებთ title ფუნციასს რადგან როგორც ვიცით ინგლისურში სათაურში ყველა სიტყვის პირველი ასო იწყება დიდი შრიფტით.
+print("Hello,",name) 
+
+
+
+
+
+
+
+
