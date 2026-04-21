81b123d54f38c8ea4b1944c6bb5a95ab8e8e5a0c
nikushatavartkiladze13 <nikatavartkiladze09@icloud.com>
2026-04-21 20:49:00 +0400

    Second commit

--- day_06/homework/hw04.py
+++ day_06/homework/hw04.py
@@ -1,17 +1,22 @@
 # dollars_to_float - ფუნქციამ უნდა მიიღოს str ტიპის მნიშვნელობა როგორც არგუმენტი და ფორმატირებული ამ მაგალითის მიხედვით $##.## სადაც ყოველი # ნიშანი რიცხვს წარმოადგენს. უნდა წაუშალოს დოლარის ნიშანი $ და დააბრუნოს მნიშნველობა როგორც float რიცხვი. ასე მაგალითად - თუ გადავეცით $50.00, ფუნქციამ უნდა დააბრუნოს 50.0
 # percent_to_float - ფუნქციამ უნდა მიიღოს str ტიპის მნიშვნელობა როგორც არგუმენტი და ფორმატირებული ამ მაგალითის მიხედვით ##% სადაც ყოველი # ნიშანი რიცხვს წარმოადგენს. უნდა წაუშალოს პროცენტის ნიშანი % და დააბრუნოს მნიშნველობა როგორც float რიცხვი. ასე მაგალითად - თუ გადავეცით 15%, ფუნქციამ უნდა პროცენტული მაჩვენებელის float ვერსია - ანუ 0.15.
-
 def main():
-    dollars = float(input("How much was the meal? "))
-    percent = float(input("What percentage would you like to tip? "))
-    tip = dollars * percent
+    dollars = input("How much was the meal? ")  
+    percent = input("What percentage would you like to tip? ") 
+
+    d = dollars_to_float(dollars)
+    p = percent_to_float(percent)
+
+    tip = d * p
     print(f"Leave ${tip:.2f}")
 
 
-    
+def dollars_to_float(d):
+    return float(d.replace("$", ""))
 
 
-    
+def percent_to_float(p):
+    return float(p.replace("%", "")) / 100
 
 
 main()
